b1310f620f666f6859f1a8b623b03c00c1add53b
= <=>
2019-04-25 23:43:08 +0700

    .

--- model/userdata.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE demo
-CREATE TABLE `products` (
-  `username` varchar(255),
-  `password` varchar(255),
-  `email` varchar(255),
-  `fullname` varchar(255),
-  `birthday` varchar(10)
-  `sex` varchar(3)
-) 
